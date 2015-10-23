ea2024573a02ace6702965bb3dd3552952808496
anthli <anthli1895@gmail.com>
2015-10-23 02:45:39 -0400

    Updated team.js

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ app.get('/team', (req, res) => {
   Otherwise, refer to the main team page.
   */
   if (member && members.indexOf(member) >= 0) {
-    res.render('teammates/' + 'apli', {memberx: single[0]});
+    res.render('teammates/' + 'apli', {
+      memberx: single[0]
+    });
   } else if (member && members.indexOf(member) < 0) {
     notFound404(req, res);
   } else {
