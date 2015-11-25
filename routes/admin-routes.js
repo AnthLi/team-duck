c15509b198f5e1c1f03542b061254a7d29077223
Benjamin <e>
2015-11-24 19:40:50 -0500

    Added get user route, user definition in database

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,8 +1,9 @@
 var express = require('express');
 
 var online = require('../lib/online').online; // List of online users
-
+var db = require('../lib/database.js');
 var router = express.Router(); // "Router" to separate particular points
+
 
 // List of online users
 router.get('/online', function(req, res) {
@@ -19,6 +20,31 @@ router.get('/online', function(req, res) {
     title: 'Online Users',
     online: online
   });
+});
+
+router.get('/users', function(req, res) {
+  var user = req.session.user;
+
+  if(!user){
+    req.flash('login', 'Not logged in');
+    res.redirect('/user/login');
+    return;
+  }
+   if (!user.admin) {
+    req.flash('index', "Invalid admin credentials");
+    res.redirect('/index');
+    return;
+  }
+  db.users( (err, data) => {
+     if (err) {
+          req.flash('adminControls', err);
+          res.redirect('/adminControls');
+          return;
+        }
+  });
+
+
+
 });
 
 // Admin authorization
@@ -47,5 +73,7 @@ router.get('/auth', function(req, res){
     
   res.render('admin', ''); // The user is an admin
 });
+
+
 
 module.exports = router;
