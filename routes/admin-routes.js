54600316d979726833b60352d5e405853114d225
AnthLi <anthli1895@gmail.com>
2015-12-03 17:00:07 -0500

    Changed admin routes to pull from database to check if the user is an admin or not

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -4,7 +4,7 @@ var online = require('../lib/online').online; // List of online users
 var db = require('../lib/database.js');
 var router = express.Router(); // "Router" to separate particular points
 
-// Verification process to see if the user is logged in, online, or an admin
+// Verification process to see if the user is logged in and/or online
 function verify(user, req, res) {
   if (!user) {
     req.flash('login', 'Not logged in');
@@ -12,19 +12,29 @@ function verify(user, req, res) {
     return false;
   }
 
-  if (user && !online[user.email]) {
+  if (user && !online[user.uid]) {
     req.flash('login', 'Login expired');
     delete req.session.user;
     res.redirect('/user/login');
     return false;
   }
 
-  if (!user.admin) {
-    res.redirect('/index');
-    return false;
+  return true;
+}
+
+// Verification to see if the current user is a dev
+// Devs are the only admins
+function verifyAdmin(data, user) {
+  var admin = false;
+
+  // If the current user is not a dev, they are not an admi
+  for (var i = 0; i < data.length; i++) {
+    if (user.email == data[i].email) {
+      admin = true;
+    }
   }
 
-  return true;
+  return admin;
 }
 
 ////// Start GET Requests
@@ -34,13 +44,25 @@ router.get('/online', (req, res) => {
   var user = req.session.user;
 
   if (!verify(user, req, res)) {
-    return;
+      return;
   }
 
-  res.render('online', {
-    title: 'Online Users',
-    online: online
-  });
+  db.team((err, data) => {
+    if (err) {
+      res.redirect('/index');
+      return;
+    }
+
+    if (!verifyAdmin(data, user)) {
+      res.redirect('/index');
+      return;
+    }
+
+    res.render('online', {
+      title: 'Online Users',
+      online: online
+    });
+  });  
 });
 
 // List of all users in the database and their attributes
@@ -51,18 +73,30 @@ router.get('/users', (req, res) => {
     return;
   }
 
-  db.users((err, data) => {
+  db.team((err, data) => {
     if (err) {
-      req.flash('userList', err);
-      res.redirect('userList');
+      res.redirect('/index');
       return;
     }
 
-    res.render('userList', {
-      title: 'Users in Database',
-      users: data
+    if (!verifyAdmin(data, user)) {
+      res.redirect('/index');
+      return;
+    }
+
+    db.users((err, data) => {
+      if (err) {
+        req.flash('userList', err);
+        res.redirect('userList');
+        return;
+      }
+
+      res.render('userList', {
+        title: 'Users in Database',
+        users: data
+      });
     });
-  });
+  });  
 });
 
 // Admin controls page
@@ -73,8 +107,20 @@ router.get('/controls', (req, res) => {
     return;
   }
 
-  res.render('adminControls', {
-    title: 'Admin Controls'
+  db.team((err, data) => {
+    if (err) {
+      res.redirect('/index');
+      return;
+    }
+
+    if (!verifyAdmin(data, user)) {
+      res.redirect('/index');
+      return;
+    }
+
+    res.render('adminControls', {
+      title: 'Admin Controls'
+    });
   });
 });
 
@@ -86,8 +132,20 @@ router.get('/classes', (req, res) => {
     return;
   }
 
-  res.render('classes', {
-    title: 'Classes'
+  db.team((err, data) => {
+    if (err) {
+      res.redirect('/index');
+      return;
+    }
+
+    if (!verifyAdmin(data, user)) {
+      res.redirect('/index');
+      return;
+    }
+
+    res.render('classes', {
+      title: 'Classes'
+    });
   });
 });
 
@@ -103,16 +161,28 @@ router.post('/auth', (req, res) => {
     return;
   }
 
-  var email = req.body.email;
-
-  db.authorizeAdmin(email, (err, user) => {
+  db.team((err, data) => {
     if (err) {
       res.redirect('/index');
       return;
     }
 
-    res.redirect('controls');
-  });
+    if (!verifyAdmin(data, user)) {
+      res.redirect('/index');
+      return;
+    }
+
+    var email = req.body.email;
+
+    db.authorizeAdmin(email, (err, user) => {
+      if (err) {
+        res.redirect('/index');
+        return;
+      }
+
+      res.redirect('controls');
+    });
+  });    
 });
 
 // Banhammer
