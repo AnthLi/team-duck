bdf7af6dc1cf4662556ed9f095b15ea9a8aae5d4
Benjamin <e>
2015-12-05 16:02:19 -0500

    nothing

---
+++ routes/class-routes.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var db = require('../lib/database.js'); // Database library
+var online = require('../lib/online').online; // List of online users
+var user = require('../lib/user.js'); // User library
+
+var router = express.Router(); // "Router" to separate particular points
